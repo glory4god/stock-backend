213e753c45a7b1a00c6ca4b89bdc64b419100c59
Hayoung <gkduddy0814@gmail.com>
2021-05-07 22:44:35 +0900

    fix : 테스트 코드 성공!

--- src/test/java/com/stock/spring/domain/company/CompanyInfoTest.java
+++ src/test/java/com/stock/spring/domain/company/CompanyInfoTest.java
@@ -5,13 +5,13 @@ import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@SpringBootTest
 public class CompanyInfoTest {
 
     @Autowired
