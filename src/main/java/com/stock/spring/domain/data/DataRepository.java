6e08c666ff61c2334f583b22e7ad4a2348faa6d0
choieunii <ghk784@dgu.ac.kr>
2021-05-16 12:45:38 +0900

    fix : 날짜에 따라 data 받아오도록 쿼리문 수정

--- src/main/java/com/stock/spring/domain/data/DataRepository.java
+++ src/main/java/com/stock/spring/domain/data/DataRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+
 public interface DataRepository extends JpaRepository<Data, String> {
-    @Query("SELECT d FROM Data d WHERE d.date= :date")
-    List<Data> dataListByDate(String date);
+    @Query("SELECT d FROM Data d WHERE DATE(d.date) >= DATE(:startDate) AND DATE(d.date) <= DATE(:endDate)")
+    List<Data> dataListByDate(String startDate, String endDate);
 }
