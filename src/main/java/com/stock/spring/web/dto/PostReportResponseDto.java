0ac3e0f13bcb487435be2c6d61e11dd39bc25c14
Hayoung <gkduddy0814@gmail.com>
2021-05-19 01:09:03 +0900

    feat : postreport 관련 dto 추가

---
+++ src/main/java/com/stock/spring/web/dto/PostReportResponseDto.java
@@ -0,0 +1,22 @@
+package com.stock.spring.web.dto;
+
+import com.stock.spring.domain.user.Report;
+import lombok.Getter;
+
+@Getter
+public class PostReportResponseDto {
+
+    private Long id;
+    private String userId;
+    private String companyName;
+    private String startDate;
+    private String endDate;
+
+    public PostReportResponseDto(Report entity) {
+        this.id = entity.getId();
+        this.userId = entity.getUserId();
+        this.companyName = entity.getCompanyName();
+        this.startDate = entity.getStartDate();
+        this.endDate = entity.getEndDate();
+    }
+}
---
+++ src/main/java/com/stock/spring/web/dto/PostReportSaveRequestDto.java
@@ -0,0 +1,34 @@
+package com.stock.spring.web.dto;
+
+import com.stock.spring.domain.user.Report;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class PostReportSaveRequestDto {
+
+    private String userId;
+    private String companyName;
+    private String startDate;
+    private String endDate;
+
+    @Builder
+    public PostReportSaveRequestDto(Report entity) {
+        this.userId = entity.getUserId();
+        this.companyName = entity.getCompanyName();
+        this.startDate = entity.getStartDate();
+        this.endDate = entity.getEndDate();
+    }
+
+    public Report toEntity() {
+        return Report.builder()
+                .userId(userId)
+                .companyName(companyName)
+                .startDate(startDate)
+                .endDate(endDate)
+                .build();
+    }
+
+}
