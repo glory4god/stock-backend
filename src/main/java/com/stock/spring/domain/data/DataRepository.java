b35353f72d2bd4097ca2d27ae7cb5af10632a13c
Hayoung <gkduddy0814@gmail.com>
2021-05-22 08:48:41 +0900

    feat : 회사별 처음 날짜와 마지막 날짜 받아오는 쿼리 추가

--- src/main/java/com/stock/spring/domain/data/DataRepository.java
+++ src/main/java/com/stock/spring/domain/data/DataRepository.java
@@ -7,6 +7,13 @@ import java.util.List;
 
 
 public interface DataRepository extends JpaRepository<Data, String> {
+
     @Query("SELECT d FROM Data d WHERE DATE(d.date) >= DATE(:startDate) AND DATE(d.date) <= DATE(:endDate)")
     List<Data> dataListByDate(String startDate, String endDate);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM Data d WHERE d.company_name=?1 ORDER BY d.date LIMIT 1")
+    Data dateStartByCompany(String company);
+
+    @Query(nativeQuery = true,value = "SELECT * FROM Data d WHERE d.company_name=?1 ORDER BY d.date DESC LIMIT 1")
+    Data dateEndByCompany(String company);
 }
