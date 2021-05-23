918dcb885e92b1cd5a8a22bd453c65981a4cb024
choieunii <ghk784@dgu.ac.kr>
2021-05-23 16:00:23 +0900

    feat : login 및 회원가입 기능

---
+++ src/main/java/com/stock/spring/service/UserService.java
@@ -0,0 +1,49 @@
+package com.stock.spring.service;
+
+import com.stock.spring.domain.user.UserInfo;
+import com.stock.spring.domain.user.UserRepository;
+import com.stock.spring.web.dto.UserInfoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * Spring Security 필수 메소드 구현
+     *
+     * @param email 이메일
+     * @return UserDetails
+     * @throws UsernameNotFoundException 유저가 없을 때 예외 발생
+     */
+    @Override // 기본적인 반환 타입은 UserDetails, UserDetails를 상속받은 UserInfo로 반환 타입 지정 (자동으로 다운 캐스팅됨)
+    public UserInfo loadUserByUsername(String email) throws UsernameNotFoundException { // 시큐리티에서 지정한 서비스이기 때문에 이 메소드를 필수로 구현
+        UserInfo userInfo = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException((email)));
+        return userInfo;
+    }
+
+    /**
+     * 회원정보 저장
+     *
+     * @param infoDto 회원정보가 들어있는 DTO
+     * @return 저장되는 회원의 PK
+     */
+
+    public Long save(UserInfoDto infoDto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        infoDto.setPassword(encoder.encode(infoDto.getPassword()));
+
+        return userRepository.save(UserInfo.builder()
+                .email(infoDto.getEmail())
+                .auth(infoDto.getAuth())
+                .password(infoDto.getPassword()).build()).getCode();
+    }
+
+}
