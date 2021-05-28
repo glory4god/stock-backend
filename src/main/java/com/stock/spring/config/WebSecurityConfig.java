ab7e7697eda64d5fafb625ffe685377fdb16e2be
Hayoung <gkduddy0814@gmail.com>
2021-05-28 12:05:13 +0900

    fix : api test 가능하도록 접근페이지 변경

--- src/main/java/com/stock/spring/config/WebSecurityConfig.java
+++ src/main/java/com/stock/spring/config/WebSecurityConfig.java
@@ -26,8 +26,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
     protected void configure(HttpSecurity http) throws Exception { // 5
         http
                 .authorizeRequests() // 6
-                .antMatchers("/login", "/signup", "/user").permitAll() // 누구나 접근 허용
-                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
+                .antMatchers("/login", "/signup", "/user","/api/**").permitAll() // 누구나 접근 허용
+                .antMatchers("/user").hasRole("USER") // USER, ADMIN만 접근 가능
                 .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
