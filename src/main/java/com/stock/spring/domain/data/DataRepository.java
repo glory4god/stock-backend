dc877ccab159c3b38741582f89c6d7473cdce8e0
Hayoung <gkduddy0814@gmail.com>
2021-05-28 12:08:33 +0900

    feat : 쿼리문에 companyName parameter 추가

--- src/main/java/com/stock/spring/domain/data/DataRepository.java
+++ src/main/java/com/stock/spring/domain/data/DataRepository.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 public interface DataRepository extends JpaRepository<Data, String> {
 
-    @Query("SELECT d FROM Data d WHERE DATE(d.date) >= DATE(:startDate) AND DATE(d.date) <= DATE(:endDate)")
-    List<Data> dataListByDate(String startDate, String endDate);
+    //    @Query("SELECT d FROM Data d WHERE (DATE(d.date) >= DATE(:startDate) AND DATE(d.date) <= DATE(:endDate))")
+    @Query("SELECT d FROM Data d WHERE company_name=?1 AND (date BETWEEN ?2 AND ?3) ORDER BY d.date")
+    List<Data> dataListByDate(String companyName,String startDate, String endDate);
 
     @Query(nativeQuery = true,value = "SELECT * FROM Data d WHERE d.company_name=?1 ORDER BY d.date LIMIT 1")
     Data dateStartByCompany(String company);
