15af9adfb2af0ef4c20424d3722b123ccb7612b6
Hayoung <gkduddy0814@gmail.com>
2021-06-04 20:30:33 +0900

    feat : add getNews feature from naverapi

--- src/main/java/com/stock/spring/service/NewsApiGetService.java
+++ src/main/java/com/stock/spring/service/NewsApiGetService.java
@@ -3,11 +3,14 @@ package com.stock.spring.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RequiredArgsConstructor
@@ -19,7 +22,7 @@ public class NewsApiGetService {
         String clientSecret = "IALrdMLh3d";//애플리케이션 클라이언트 시크릿값";
         try {
             String text = URLEncoder.encode(keyword, "UTF-8");
-            String apiURL = "https://openapi.naver.com/v1/search/news.json?query="+text+"&display=10&sort=sim";
+            String apiURL = "https://openapi.naver.com/v1/search/news.json?query="+text+"&display=18&sort=sim";
             URL url = new URL(apiURL);
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
             con.setRequestMethod("GET");
@@ -47,30 +50,87 @@ public class NewsApiGetService {
         }
     }
 
-//    public static void main(String[] args)  {
-//
-//        String clientId = "VgK9ERVBz6_4f9p3RDzo";//애플리케이션 클라이언트 아이디값";
-//        String clientSecret = "IALrdMLh3d";//애플리케이션 클라이언트 시크릿값";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("X-Naver-Client-Id", clientId);
-//        headers.set("X-Naver-Client-Secret", clientSecret);
-//
-//        HttpEntity request = new HttpEntity(headers);
-//        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl("https://openapi.naver.com/v1/search/news.json")
-//                .queryParam("query", "삼성")
-//                .queryParam("display", 10)
-//                .queryParam("sort", "sim");
-//
-//        ResponseEntity<String> response = restTemplate.exchange(
-//                uriBuilder.toUriString(),
-//                HttpMethod.GET,
-//                request,
-//                String.class
-//        );
-//        System.out.println(response.toString());
-//
-//    }
+    public void getKeywords(String keyword) {
+        String clientId = "VgK9ERVBz6_4f9p3RDzo";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "IALrdMLh3d";//애플리케이션 클라이언트 시크릿값";
+        String startDate = "2021-05-28";
+        String endDate = "2021-06-01";
+        String timeUnit = "date";
 
+        String apiUrl = "https://openapi.naver.com/v1/datalab/search";
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        requestHeaders.put("Content-Type", "application/json");
+
+        String requestBody = "{\"startDate\":\"2017-01-01\"," +
+                "\"endDate\":\"2017-04-30\"," +
+                "\"timeUnit\":\"month\"," +
+                "\"keywordGroups\":[{\"groupName\":\"한글\"," + "\"keywords\":[\"한글\",\"korean\"]}," +
+                "{\"groupName\":\"영어\"," + "\"keywords\":[\"영어\",\"english\"]}]," +
+                "\"device\":\"pc\"," +
+                "\"ages\":[\"1\",\"2\"]," +
+                "\"gender\":\"f\"}";
+
+        String responseBody = post(apiUrl, requestHeaders, requestBody);
+        System.out.println(responseBody);
+    }
+
+    private static String post(String apiUrl, Map<String, String> requestHeaders, String requestBody) {
+        HttpURLConnection con = connect(apiUrl);
+
+        try {
+            con.setRequestMethod("POST");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            con.setDoOutput(true);
+            try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(requestBody.getBytes());
+                wr.flush();
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
+                return readBody(con.getInputStream());
+            } else {  // 에러 응답
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect(); // Connection을 재활용할 필요가 없는 프로세스일 경우
+        }
+    }
+
+    private static HttpURLConnection connect(String apiUrl) {
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection) url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private static String readBody(InputStream body) {
+        InputStreamReader streamReader = new InputStreamReader(body, StandardCharsets.UTF_8);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
 
 }
--- src/main/java/com/stock/spring/web/ApiController.java
+++ src/main/java/com/stock/spring/web/ApiController.java
@@ -1,41 +1,43 @@
 package com.stock.spring.web;
 
 import com.stock.spring.service.GetService;
-import com.stock.spring.web.dto.GetCompanyNameResponseDto;
-import com.stock.spring.web.dto.GetCompanyResponseDto;
-import com.stock.spring.web.dto.GetDateRangeResponseDto;
-import com.stock.spring.web.dto.GetDateResponseDto;
+import com.stock.spring.service.NewsApiGetService;
+import com.stock.spring.web.dto.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
 public class ApiController {
     private final GetService getService;
+    private final NewsApiGetService newsApiGetService;
 
     @GetMapping("/api/stock/companyname/{market}")
-    public List<GetCompanyNameResponseDto> CompanyNameListByMarket(@PathVariable String market) {
+    public List<String> CompanyNameListByMarket(@PathVariable String market) {
         return getService.CompanyNameListByMarket(market);
     }
 
     @GetMapping("/api/stock/company/{market}")
-    public List<GetCompanyResponseDto> CompanyListByMarket(@PathVariable String market) {
+    public List<GetCompanyNameResponseDto> CompanyListByMarket(@PathVariable String market) {
         return getService.CompanyListByMarket(market);
     }
 
     @GetMapping("/api/stock/data/condition/{companyName}/")
     public List<GetDateResponseDto> dataListByDate(@PathVariable String companyName, @RequestParam(value = "start") String startDate, @RequestParam(value = "end") String endDate) {
         System.out.println(startDate + " " + endDate);
-        return getService.dataListByDate(companyName,startDate, endDate);
+        return getService.dataListByDate(companyName, startDate, endDate);
     }
 
     @GetMapping("/api/stock/data/{companyName}")
     public List<GetDateRangeResponseDto> dateRangeByCompany(@PathVariable String companyName) {
         return getService.dateRangeByCompany(companyName);
+    }
+
+    @GetMapping("/api/finance/news/{keyword}")
+    public Object getNews(@PathVariable String keyword) {
+        return newsApiGetService.getNews(keyword);
     }
 }
