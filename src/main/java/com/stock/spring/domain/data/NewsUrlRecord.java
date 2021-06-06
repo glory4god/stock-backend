978bcd6ef31f6875cb2f3ad50ba6e9160e654db2
Hayoung <gkduddy0814@gmail.com>
2021-06-07 00:42:26 +0900

    feat : news url 클릭시 데이터 저장 Entity & repository

---
+++ src/main/java/com/stock/spring/domain/data/NewsUrlRecord.java
@@ -0,0 +1,28 @@
+package com.stock.spring.domain.data;
+
+import com.stock.spring.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "news_url_record")
+public class NewsUrlRecord extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Builder
+    public NewsUrlRecord(String url) {
+        this.url = url;
+    }
+
+}
