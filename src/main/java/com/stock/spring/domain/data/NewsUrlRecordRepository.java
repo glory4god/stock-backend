a2cfbede79ae9a0f23f018b9dc110d91b773c59b
Hayoung <gkduddy0814@gmail.com>
2021-06-09 01:19:04 +0900

    feat: url 개수 순으로 꺼내는 쿼리 추가

--- src/main/java/com/stock/spring/domain/data/NewsUrlRecordRepository.java
+++ src/main/java/com/stock/spring/domain/data/NewsUrlRecordRepository.java
@@ -1,7 +1,12 @@
 package com.stock.spring.domain.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NewsUrlRecordRepository extends JpaRepository<NewsUrlRecord, String> {
 
+    @Query("SELECT u from NewsUrlRecord u GROUP BY u.link ORDER BY COUNT(u.link) DESC")
+    List<NewsUrlRecord> getPopularNews();
 }
