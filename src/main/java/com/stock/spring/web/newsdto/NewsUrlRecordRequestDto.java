842dc5b30da5cd7e959cd6da9673e6927cca2ba0
Hayoung <gkduddy0814@gmail.com>
2021-06-09 01:17:24 +0900

    feat: add NewsUrlRecord entity & dto

---
+++ src/main/java/com/stock/spring/web/newsdto/NewsUrlRecordRequestDto.java
@@ -0,0 +1,37 @@
+package com.stock.spring.web.newsdto;
+
+import com.stock.spring.domain.data.NewsUrlRecord;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class NewsUrlRecordRequestDto {
+
+    private String title;
+    private String link;
+    private String originallink;
+    private String pubDate;
+    private String description;
+
+    @Builder
+    public NewsUrlRecordRequestDto(String title, String link,String originallink,String pubDate, String description) {
+        this.title = title;
+        this.link = link;
+        this.originallink = originallink;
+        this.description = description;
+        this.pubDate = pubDate;
+    }
+
+    public NewsUrlRecord toEntity() {
+        return NewsUrlRecord.builder()
+                .title(title)
+                .link(link)
+                .originallink(originallink)
+                .pubDate(pubDate)
+                .description(description)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/stock/spring/web/newsdto/NewsUrlRecordResponseDto.java
@@ -0,0 +1,22 @@
+package com.stock.spring.web.newsdto;
+
+import com.stock.spring.domain.data.NewsUrlRecord;
+import lombok.Getter;
+
+@Getter
+public class NewsUrlRecordResponseDto {
+
+    private String title;
+    private String link;
+    private String originallink;
+    private String description;
+    private String pubDate;
+
+    public NewsUrlRecordResponseDto(NewsUrlRecord entity) {
+        this.title = entity.getTitle();
+        this.link = entity.getLink();
+        this.originallink = entity.getOriginallink();
+        this.description = entity.getDescription();
+        this.pubDate = entity.getPubDate();
+    }
+}
