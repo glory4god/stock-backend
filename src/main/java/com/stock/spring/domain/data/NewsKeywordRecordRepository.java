4c7a0060f55e34d0782cba4efec13864b42d92fa
Hayoung <gkduddy0814@gmail.com>
2021-06-15 23:05:25 +0900

    feat : add get keywordrecord query

--- src/main/java/com/stock/spring/domain/data/NewsKeywordRecordRepository.java
+++ src/main/java/com/stock/spring/domain/data/NewsKeywordRecordRepository.java
@@ -2,6 +2,12 @@ package com.stock.spring.domain.data;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NewsKeywordRecordRepository extends JpaRepository<NewsKeywordRecord,String> {
+
+    @Query("SELECT k from NewsKeywordRecord k GROUP BY k.keyword ORDER BY COUNT(k.keyword) DESC")
+    List<NewsKeywordRecord> getPopularKeyword();
 }
