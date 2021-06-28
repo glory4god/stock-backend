acb5ff4114ec68f9aff8bde5296c7f7baa240288
Hayoung <gkduddy0814@gmail.com>
2021-06-28 18:23:46 +0900

    feat : add query get news by daily and weekly

--- src/main/java/com/stock/spring/domain/data/NewsUrlRecordRepository.java
+++ src/main/java/com/stock/spring/domain/data/NewsUrlRecordRepository.java
@@ -12,4 +12,12 @@ public interface NewsUrlRecordRepository extends JpaRepository<NewsUrlRecord, St
 
     @Query("SELECT u from NewsUrlRecord u WHERE u.keyword=?1 GROUP BY u.link ORDER BY COUNT(u.link) DESC")
     List<NewsUrlRecord> getPopularNewsByKeyword(String keyword);
+
+    //오늘 인기순위
+    @Query("SELECT u from NewsUrlRecord u WHERE DATE_FORMAT(create_date, '%Y-%m-%d')=DATE_FORMAT(now(),'%Y-%m-%d') GROUP BY u.keyword ORDER BY COUNT(u.keyword) DESC")
+    List<NewsUrlRecord> getPopularNewsByDaily();
+
+    //일주일 인기순위
+    @Query("SELECT u from NewsUrlRecord u WHERE (create_date BETWEEN ?2 AND ?1) GROUP BY u.keyword ORDER BY COUNT(u.keyword) DESC")
+    List<NewsUrlRecord> getPopularNewsByWeekly(String currentTime, String intervalWeekTime);
 }
