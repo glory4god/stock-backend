532bcaa5ec670c9089c322760f4d1a272cb0290f
Hayoung <gkduddy0814@gmail.com>
2021-07-04 01:45:40 +0900

    fix : test commit

--- src/main/java/com/stock/spring/websocket/domain/Message.java
+++ src/main/java/com/stock/spring/websocket/domain/Message.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "username", nullable = false)
