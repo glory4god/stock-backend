6851bbe8503fa7714303416579f469f62c7a3b6d
Hayoung <gkduddy0814@gmail.com>
2021-07-03 14:35:19 +0900

    feat : add chat service

---
+++ src/main/java/com/stock/spring/websocket/service/ChatService.java
@@ -0,0 +1,33 @@
+package com.stock.spring.websocket.service;
+
+import com.stock.spring.websocket.domain.Message;
+import com.stock.spring.websocket.domain.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatService {
+    private final MessageRepository messageRepository;
+
+    @Transactional
+    public Message saveChat(Message message) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        message.setDate(LocalDateTime.now().format(formatter));
+        Message entity = messageRepository.save(message);
+        System.out.println(entity);
+        return entity;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Message> findAll() {
+        return messageRepository.findAll();
+    }
+}
