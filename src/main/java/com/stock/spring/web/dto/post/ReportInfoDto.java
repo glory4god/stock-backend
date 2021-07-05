6a2b2f92355521c2500d35a1adf4aee9c7788fff
Hayoung <gkduddy0814@gmail.com>
2021-07-06 01:34:31 +0900

    feat : add reportinfodto

---
+++ src/main/java/com/stock/spring/web/dto/post/ReportInfoDto.java
@@ -0,0 +1,26 @@
+package com.stock.spring.web.dto.post;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ReportInfoDto {
+    private String title;
+    private String content;
+    private LocalDateTime createDate;
+    private LocalDateTime modifiedDate;
+    private int good;
+    private int bad;
+
+    public ReportInfoDto(String title, String content, LocalDateTime createDate, LocalDateTime modifiedDate, int good
+            , int bad) {
+        this.title = title;
+        this.content = content;
+        this.createDate = createDate;
+        this.modifiedDate = modifiedDate;
+        this.good = good;
+        this.bad = bad;
+
+    }
+}
