ab89f4af40c70a04106de066d647c9adc4f69d2a
Hayoung <gkduddy0814@gmail.com>
2021-07-08 00:27:50 +0900

    feat : add put method

--- src/main/java/com/stock/spring/domain/user/ChartReportRepository.java
+++ src/main/java/com/stock/spring/domain/user/ChartReportRepository.java
@@ -1,11 +1,24 @@
 package com.stock.spring.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ChartReportRepository extends JpaRepository<ChartReport, String> {
 
-    @Query("SELECT r FROM ChartReport r WHERE id=?1")
+    @Query("SELECT c FROM ChartReport c WHERE id=?1")
     ChartReport getReportById(Long id);
-}
 
+    @Modifying
+    @Query("update ChartReport c set c.good=?2 where c.id=?1")
+    int updateGoodById(Long id, int good);
+
+    @Modifying
+    @Query("update ChartReport c set c.views=?2 where c.id=?1")
+    int updateViewsById(Long id, int views);
+
+    @Modifying
+    @Query("update ChartReport c set c.bad=?2 where c.id=?1")
+    int updateBadById(Long id, int bad);
+
+}
