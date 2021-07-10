b1a1ba68e09263bc559b41ca3bb82cfa8b585eb1
Hayoung <gkduddy0814@gmail.com>
2021-07-11 01:55:40 +0900

    feat : add findByName feature

--- src/main/java/com/stock/spring/domain/user/ChartReportRepository.java
+++ src/main/java/com/stock/spring/domain/user/ChartReportRepository.java
@@ -1,8 +1,11 @@
 package com.stock.spring.domain.user;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChartReportRepository extends JpaRepository<ChartReport, String> {
 
@@ -20,5 +23,12 @@ public interface ChartReportRepository extends JpaRepository<ChartReport, String
     @Modifying
     @Query("update ChartReport c set c.bad=?2 where c.id=?1")
     int updateBadById(Long id, int bad);
+
+    
+    // Sort 넘겨서 정렬 좀 더 쉽게 할 수 있음!!!
+    // 근데 WHERE 절도 간단하게 할 수 있을텐데 잘 모르겠다 ㅠㅠ
+    @Query("SELECT c FROM ChartReport c WHERE companyName=?1")
+    List<ChartReport> findByName(String companyName, Sort sort);
+
 
 }
