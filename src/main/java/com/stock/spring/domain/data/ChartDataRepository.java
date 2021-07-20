f13785d210eea0735ef6f558720fe90c4aabee8d
Hayoung <gkduddy0814@gmail.com>
2021-07-21 00:38:13 +0900

    fix : change table name

--- src/main/java/com/stock/spring/domain/data/ChartDataRepository.java
+++ src/main/java/com/stock/spring/domain/data/ChartDataRepository.java
@@ -12,9 +12,9 @@ public interface ChartDataRepository extends JpaRepository<ChartData, String> {
     @Query("SELECT d FROM ChartData d WHERE company_name=?1 AND (date BETWEEN ?2 AND ?3) ORDER BY d.date")
     List<ChartData> dataListByDate(String companyName, String startDate, String endDate);
 
-    @Query(nativeQuery = true,value = "SELECT * FROM ChartData WHERE company_name=?1 ORDER BY date LIMIT 1")
+    @Query(nativeQuery = true,value = "SELECT * FROM chart_data WHERE company_name=?1 ORDER BY date LIMIT 1")
     ChartData dateStartByCompany(String company);
 
-    @Query(nativeQuery = true,value = "SELECT * FROM ChartData WHERE company_name=?1 ORDER BY date DESC LIMIT 1")
+    @Query(nativeQuery = true,value = "SELECT * FROM chart_data WHERE company_name=?1 ORDER BY date DESC LIMIT 1")
     ChartData dateEndByCompany(String company);
 }
