c6f6c6cce101c9754acbc22cb3371292e84d82d0
Hayoung <gkduddy0814@gmail.com>
2021-07-21 00:40:12 +0900

    feat : add kakao profile image data

--- src/main/java/com/stock/spring/kakaoOAuth/KakaoUserResponseDto.java
+++ src/main/java/com/stock/spring/kakaoOAuth/KakaoUserResponseDto.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 public class KakaoUserResponseDto {
     private Long id;
     private String nickname;
+    private String image;
     private String access_token;
 
     @Builder
-    public KakaoUserResponseDto(Long id, String nickname, String access_token) {
+    public KakaoUserResponseDto(Long id, String nickname,String image, String access_token) {
         this.id = id;
         this.nickname = nickname;
+        this.image = image;
         this.access_token = access_token;
     }
 }
