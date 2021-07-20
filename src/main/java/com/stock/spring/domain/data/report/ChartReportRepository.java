ae248f5466a9ce64abdcb37096eabd6056153eab
Hayoung <gkduddy0814@gmail.com>
2021-07-21 00:39:19 +0900

    feat : add sort feature by username, companyname, title&content

--- src/main/java/com/stock/spring/domain/data/report/ChartReportRepository.java
+++ src/main/java/com/stock/spring/domain/data/report/ChartReportRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ChartReportRepository extends JpaRepository<ChartReport, String> {
+public interface ChartReportRepository extends JpaRepository<ChartReport, Long> {
 
     @Query("SELECT c FROM ChartReport c WHERE id=?1")
     ChartReport getReportById(Long id);
@@ -24,11 +24,23 @@ public interface ChartReportRepository extends JpaRepository<ChartReport, String
     @Query("update ChartReport c set c.bad=?2 where c.id=?1")
     int updateBadById(Long id, int bad);
 
-    
+
     // Sort 넘겨서 정렬 좀 더 쉽게 할 수 있음!!!
     // 근데 WHERE 절도 간단하게 할 수 있을텐데 잘 모르겠다 ㅠㅠ
     @Query("SELECT c FROM ChartReport c WHERE companyName=?1")
     List<ChartReport> findByName(String companyName, Sort sort);
+
+    @Query("SELECT c FROM ChartReport c WHERE username=?1")
+    List<ChartReport> findAllByUsername(String nickname);
+
+    @Query("SELECT c FROM ChartReport c WHERE username LIKE %?1%")
+    List<ChartReport> searchListByUsername(String username, Sort sort);
+
+    @Query("SELECT c FROM ChartReport c WHERE companyName LIKE %?1%")
+    List<ChartReport> searchListByCompanyName(String companyName, Sort sort);
+
+    @Query("SELECT c FROM ChartReport c WHERE (title LIKE %?1% AND content LIKE %?1%)")
+    List<ChartReport> searchListByTitleAndContent(String value, Sort sort);
 
 
 }
