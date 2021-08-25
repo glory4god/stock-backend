14c96849d5b880c8f8cc069d0b9b0ab7be9b38a4
Hayoung <gkduddy0814@gmail.com>
2021-08-25 13:01:07 +0900

    feat : add responseDto

---
+++ src/main/java/com/stock/spring/web/dto/post/FreeBoardReponseDto.java
@@ -0,0 +1,30 @@
+package com.stock.spring.web.dto.post;
+
+import com.stock.spring.domain.data.freeBoard.FreeBoard;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class FreeBoardReponseDto {
+    private Long id;
+    private String username;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private int good;
+    private int views;
+
+    public FreeBoardReponseDto(FreeBoard entity) {
+        this.id = entity.getId();
+        this.username = entity.getUsername();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.good = entity.getGood();
+        this.views = entity.getViews();
+        this.createdDate = entity.getCreatedDate();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+
+}
